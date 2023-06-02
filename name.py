b4f385cfe43ba6e0367d0a1902fdc3e98974b055
SiarheiZ <szhachek@gmail.com>
2023-06-03 02:36:11 +0300

    second_task_done

--- name.py
+++ name.py
@@ -1,6 +1,6 @@
 def name():
     while True:
-        input_name = input('Enter name: ')
+        input_name = input('Введите имя: ')
         if input_name != 'Вячеслав':
             print('Нет такого имени')
         else:
