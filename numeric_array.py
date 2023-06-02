8b683a6db4942c08c9e69717c99ccbdb4f89633d
SiarheiZ <szhachek@gmail.com>
2023-06-03 02:32:21 +0300

    third_task_done

--- numeric_array.py
+++ numeric_array.py
@@ -1,10 +1,14 @@
 def multiple_of_three():
     num = input('Enter the numbers separated by a space: ')
     num_list = list(num.split(' '))
+    count = 0
     for digit in num_list:
         digit = int(digit)
         if digit % 3 == 0:
-            print(digit, end=' ')
+            count += 1
+            print(f'Numbers multiples of three: {digit}', end=' ')
+    if count == 0:
+        print('No numbers multiples of three.')
 
 
 multiple_of_three()
